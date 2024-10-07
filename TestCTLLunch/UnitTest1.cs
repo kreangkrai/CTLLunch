448d6f3f81109fb982f5589cac7cadccd49f3fe3
kreangkrai <kreangkrai.ratt@gmail.com>
2024-10-07 15:42:17 +0700

    xxxx

---
+++ TestCTLLunch/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCTLLunch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
