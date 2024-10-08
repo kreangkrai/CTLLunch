db31497f491ea794a06467fa06cb242b91280271
kreangkrai <kreangkrai.ratt@gmail.com>
2024-10-08 13:01:44 +0700

    update

--- TestCTLLunch/UnitTest1.cs
+++ TestCTLLunch/UnitTest1.cs
@@ -1,19 +1,28 @@
 using CTLLunch.Interface;
+using CTLLunch.Models;
+using CTLLunch.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 namespace TestCTLLunch
 {
     [TestClass]
     public class UnitTest1
     {
         private IReserve Reserve;
-        public UnitTest1(IReserve _Reserve)
+        private IEmployee Employee;
+        public UnitTest1(IReserve _Reserve, IEmployee _Employee)
         {
              Reserve = _Reserve;
+             Employee = _Employee;
         }
+        
         [TestMethod]
-        public void TestMethod1()
+        
+        public void TestMethod()
         {
+            List<EmployeeModel> employees = Employee.GetEmployees();
+
+            Assert.AreEqual(1, employees.Count);
         }
     }
 }
